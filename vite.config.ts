0f5e87ac08545d7adfca16c6cdfb3be915fab054
6-keem <6ukeem@gmail.com>
2025-01-16 18:21:38 +0900

    feat: add shadcn/ui components and set alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,10 +10,10 @@ export default defineConfig({
       // 여러 엔트리를 정의
       input: {
         // background.ts, content.ts의 위치를 지정
-        background: resolve(__dirname, 'src/background.ts'),
-        content: resolve(__dirname, 'src/content.ts'),
+        background: path.resolve(__dirname, 'src/background.ts'),
+        content: path.resolve(__dirname, 'src/content.ts'),
         // React 앱(entry point)도 예시로 함께 넣을 수 있음
-        popup: resolve(__dirname, 'index.html'),
+        popup: path.resolve(__dirname, 'index.html'),
       },
       output: {
         // 빌드시 생성될 파일 이름 패턴
@@ -22,5 +22,10 @@ export default defineConfig({
     },
     outDir: 'dist', // 빌드 결과물 폴더
     sourcemap: false, // 필요에 따라 소스맵 사용 여부 설정
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
